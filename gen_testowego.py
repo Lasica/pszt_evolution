ffeb82984e99ac718c62628fc237aecea7fc3f4c
boxi01 <45149847+boxi01@users.noreply.github.com>
2018-11-24 15:57:40 +0100

    dodano objęctość plecaka

--- gen_testowego.py
+++ gen_testowego.py
@@ -6,13 +6,17 @@ import argparse
 parser = argparse.ArgumentParser(description="Generator testów na rzecz projektów z PSZT")
 parser.add_argument('--filename', help="Ścieżka pliku testowego", type=str, default="testowy.csv")
 parser.add_argument('licz_przedmiotow', help="Liczba przedmiotów które wygenerujemy")
+parser.add_argument('pojemnosc_plecaka', help="Pojemność naszego plecaka", type=int, default=50)
 parser.add_argument('--srednia', help="Średnia rozkładu normalnego", type=float, default=10, required=False)
 parser.add_argument('--wariancja', help='Wariancja rozkładu normalnego', type=float, default=2, required=False)
+
+
 
 args = parser.parse_args()
 
 file_path = args.filename
 N = args.licz_przedmiotow
+v = args.pojemnosc_plecaka
 mu = args.srednia
 sigma = args.wariancja
 
@@ -26,6 +30,12 @@ for x in range(0, int(N)):
 
 print(items_list)
 
-myfile = open(file_path)
+myfile = open(file_path, "w")
 
-myfile.write(N)
+myfile.write(str((N, v)))
+
+for t in items_list:
+    myfile.write(str(t))
+
+myfile.close()
+
