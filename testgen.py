a1c6913aed485b832ba15c0dd5073d77823cac55
Lasica <dobrogo.a@gmail.com>
2018-11-24 19:10:21 +0100

    dodanie opcji nwd do generatora testow

--- testgen.py
+++ testgen.py
@@ -5,12 +5,13 @@ import numpy as np
 import argparse
 import yaml
 
-parser = argparse.ArgumentParser(description="Generator testów na rzecz projektów z PSZT")
-parser.add_argument('--filename', help="Ścieżka pliku testowego", type=str, default="testowy.csv")
-parser.add_argument('licz_przedmiotow', help="Liczba przedmiotów które wygenerujemy", type=int)
-parser.add_argument('pojemnosc_plecaka', help="Pojemność naszego plecaka", type=int, default=50)
-parser.add_argument('--srednia', help="Średnia rozkładu normalnego", type=float, default=10, required=False)
-parser.add_argument('--wariancja', help='Wariancja rozkładu normalnego', type=float, default=2, required=False)
+parser = argparse.ArgumentParser(description="Generator testow na rzecz projektu z PSZT")
+parser.add_argument('--filename', help="Sciezka pliku testowego", type=str, default="testowy.in")
+parser.add_argument('licz_przedmiotow', help="Liczba przedmiotow ktore zostana wygenerowane", type=int)
+parser.add_argument('pojemnosc_plecaka', help="Pojemnosc plecaka", type=int, default=50)
+parser.add_argument('--srednia', help="Srednia rozkladu normalnego", type=float, default=10, required=False)
+parser.add_argument('--wariancja', help='Wariancja rozkladu normalnego', type=float, default=2, required=False)
+parser.add_argument('--nwd', help="Zadana najwieksza wspolna wielokrotnosc wag", type=int, default=1, required=False)
 
 
 
@@ -23,16 +24,18 @@ mu = args.srednia
 sigma = args.wariancja
 
 items_list = []
-
+nwd = args.nwd
 for x in range(0, int(N)):
     waga    = int(np.random.normal(mu, sigma, 1))
+    if(nwd > 1):
+        waga = min(nwd, int(waga/nwd)*nwd)
     wartosc = int(np.random.normal(mu, sigma, 1))
-
+    
     items_list.append((waga, wartosc))
 
 print(items_list)
 
 with open(file_path, "w") as myfile:
-    yaml.dump({"volume":v, "items_number":N, "items":items_list}, stream=myfile)
+    yaml.dump({"volume": v, "items_number": N, "items": items_list, "description": "weight/value"}, stream=myfile)
 
 
