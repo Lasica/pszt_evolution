1882b156cac88f9487c2d26fb05f841e1b56f52b
Lasica <dobrogo.a@gmail.com>
2018-11-24 16:15:58 +0100

    format zapisu - yaml

--- gen_testowego.py
+++ gen_testowego.py
@@ -1,11 +1,13 @@
+#!/bin/python
 import random
 import argparse
 import numpy as np
 import argparse
+import yaml
 
 parser = argparse.ArgumentParser(description="Generator testów na rzecz projektów z PSZT")
 parser.add_argument('--filename', help="Ścieżka pliku testowego", type=str, default="testowy.csv")
-parser.add_argument('licz_przedmiotow', help="Liczba przedmiotów które wygenerujemy")
+parser.add_argument('licz_przedmiotow', help="Liczba przedmiotów które wygenerujemy", type=int)
 parser.add_argument('pojemnosc_plecaka', help="Pojemność naszego plecaka", type=int, default=50)
 parser.add_argument('--srednia', help="Średnia rozkładu normalnego", type=float, default=10, required=False)
 parser.add_argument('--wariancja', help='Wariancja rozkładu normalnego', type=float, default=2, required=False)
@@ -30,12 +32,7 @@ for x in range(0, int(N)):
 
 print(items_list)
 
-myfile = open(file_path, "w")
+with open(file_path, "w") as myfile:
+    yaml.dump({"volume":v, "items_number":N, "items":items_list}, stream=myfile)
 
-myfile.write(str((N, v)))
-
-for t in items_list:
-    myfile.write(str(t))
-
-myfile.close()
 
