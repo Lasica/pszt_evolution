d243d041c5b7885a1ff3a83bafe038a251ed2fbf
boxi01 <45149847+boxi01@users.noreply.github.com>
2018-11-24 15:29:28 +0100

    Create gen_testowego.py
    Generator plików testowych

---
+++ gen_testowego.py
@@ -0,0 +1,32 @@
+
+import random
+import argparse
+import numpy as np
+import argparse
+
+parser = argparse.ArgumentParser(description = "Generator testów na rzecz projektów z PSZT")
+parser.add_argument('--filename', help ="Ścieżka pliku testowego", type=str, default="testowy.csv")
+parser.add_argument('licz_przedmiotow', help = "Liczba przedmiotów które wygenerujemy")
+parser.add_argument('--srednia', help = "Średnia rozkładu normalnego", type= float,  default = 10, required = False)
+parser.add_argument('--wariancja', help = 'Wariancja rozkładu normalnego', type = float,  default = 2,  required = False)
+
+args = parser.parse_args()
+
+file_path = args.filename
+N = args.licz_przedmiotow
+mu = args.srednia
+sigma = args.wariancja
+
+items_list =  []
+
+for x in range(0,int(N)):
+    waga    = int(np.random.normal(mu,sigma,1))
+    wartosc = int(np.random.normal(mu, sigma,1))
+
+    items_list.append((waga,wartosc))
+
+print(items_list)
+
+myfile = open(file_path)
+
+myfile.write(N)
