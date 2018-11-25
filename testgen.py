cc6f56205ca2e18061e96034ffc1d987a3717375
boxi01 <45149847+boxi01@users.noreply.github.com>
2018-11-25 19:03:40 +0100

    Update testgen.py
    Pdział paramertów wariancji i średniej dla wag i wartości

--- testgen.py
+++ testgen.py
@@ -9,8 +9,10 @@ parser = argparse.ArgumentParser(description="Generator testow na rzecz projektu
 parser.add_argument('--filename', help="Sciezka pliku testowego", type=str, default="testowy.in")
 parser.add_argument('licz_przedmiotow', help="Liczba przedmiotow ktore zostana wygenerowane", type=int)
 parser.add_argument('pojemnosc_plecaka', help="Pojemnosc plecaka", type=int, default=50)
-parser.add_argument('--srednia', help="Srednia rozkladu normalnego", type=float, default=10, required=False)
-parser.add_argument('--wariancja', help='Wariancja rozkladu normalnego', type=float, default=2, required=False)
+parser.add_argument('--srednia_wag', help="Srednia rozkladu normalnego dla wag", type=float, default=10, required=False)
+parser.add_argument('--srednia_wartosci', help="Srednia rozkladu normalnego dla wartosci", type=float, default=10, required=False)
+parser.add_argument('--wariancja_wag', help='Wariancja rozkladu normalnego dla wag', type=float, default=2, required=False)
+parser.add_argument('--wariancja_wartosci', help='Wariancja rozkladu normalnego dla wartosci', type=float, default=2, required=False)
 parser.add_argument('--nwd', help="Zadana najwieksza wspolna wielokrotnosc wag", type=int, default=1, required=False)
 parser.add_argument('-v', help="Tryb verbose", action="store_true")
 
@@ -19,16 +21,18 @@ args = parser.parse_args()
 file_path = args.filename
 N = args.licz_przedmiotow
 v = args.pojemnosc_plecaka
-mu = args.srednia
-sigma = args.wariancja
+mu_w = args.srednia_wag
+mu_p = args.srednia_wartosci
+sigma_w = args.wariancja_wag
+sigma_p = args.wariancja_wartosci
 
 items_list = []
 nwd = args.nwd
 for x in range(0, int(N)):
-    waga = max(int(np.random.normal(mu, sigma, 1)), 1)  # FIXME wagi i wartosci powinny moc miec inne rozklady.
+    waga = max(int(np.random.normal(mu_w, sigma_w, 1)), 1)  # FIXME wagi i wartosci powinny moc miec inne rozklady.
     if nwd > 1:
         waga = max(nwd, int(waga / nwd) * nwd)
-    wartosc = int(np.random.normal(mu, sigma, 1))
+    wartosc = int(np.random.normal(mu_p, sigma_p, 1))
 
     items_list.append((waga, wartosc))
 
