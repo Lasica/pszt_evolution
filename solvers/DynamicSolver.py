347618c47ec243c0654ad615043448f31175b316
Lasica <dobrogo.a@gmail.com>
2019-01-13 00:36:06 +0100

    Fix wyniku metody dynamicznej

--- solvers/DynamicSolver.py
+++ solvers/DynamicSolver.py
@@ -1,4 +1,5 @@
 from .VirtualSolver import VirtualSolver
+import numpy as np
 
 
 class DynamicSolver(VirtualSolver):  # dziedziczenie interfejsu, rzecz w pythonie zbedna ale porzadkuje kod
@@ -11,7 +12,6 @@ class DynamicSolver(VirtualSolver):  # dziedziczenie interfejsu, rzecz w pythoni
         return a
 
     def solve(self):
-
         if not (type(self.items[0][0]) is int and type(self.items[0][1]) is int):
             raise Exception("Wrong input type. Should be int, while it is {} {}".format(type(self.items[0][0]), type(self.items[0][1])))
 
@@ -20,23 +20,29 @@ class DynamicSolver(VirtualSolver):  # dziedziczenie interfejsu, rzecz w pythoni
             current_nwd = self.nwd(current_nwd, w)
 
         backpack_slots = int(self.capacity/current_nwd)
-        backpack = [(-1, -1)] * (backpack_slots + 1)  # symulowana nieskonczonosc
-        backpack[0] = (0, -1)
+        backpack = [-1] * (backpack_slots + 1)  # symulowana nieskonczonosc
+        taken = np.zeros((len(self.items), backpack_slots+1), dtype='uint8')
+        backpack[0] = 0
         for j, item in enumerate(self.items):  # petla po dostepnych przedmiotach
-            p = item[0]
-            w = item[1]
+            p, w = item
             for i in reversed(range(len(backpack))):  # petla po miejscach w w plecaku (od tylu)
-                if i + w/current_nwd <= backpack_slots and backpack[i][0] > -1:
-                    if backpack[i + int(w/current_nwd)][0] < backpack[i][0] + p:
-                        backpack[i + int(w/current_nwd)] = (backpack[i][0] + p, j)
+                if i + w/current_nwd <= backpack_slots and backpack[i] > -1:
+                    nidx = i+int(w/current_nwd)
+                    if backpack[nidx] < backpack[i] + p:
+                        backpack[nidx] = backpack[i] + p
+                        taken[j][nidx] = 1
 
         # odtwarzanie wyniku
         self.result = []
         prize = max(backpack)
         best_idx = backpack.index(prize)
-        while prize[0] > 0:
-            item = self.items[prize[1]]
-            self.result.append((item, prize[1]))
+        first_taken = len(self.items)
+        while prize > 0:
+            first_taken -= 1
+            while not taken[first_taken][best_idx]:
+                first_taken -= 1
+            item = self.items[first_taken]
+            self.result.append((item, first_taken))
             best_idx = best_idx - int(item[1]/current_nwd)
             prize = backpack[best_idx]
         return
