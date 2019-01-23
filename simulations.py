a4a26fa07286315a602fc744510ddeb7e84c9ea8
alr94AR <alan.n.rashid@gmail.com>
2019-01-23 23:29:57 +0100

    do testów ze zwracaniem

--- simulations.py
+++ simulations.py
@@ -1,4 +1,9 @@
 import subprocess
 import os
 
-os.system("python3 main.py --algorithm all --parameters param1.conf tests\\01test_20_100.in ")
+base_cmd = 'python3 main.py --algorithm all --parameters conf\\param'
+base_cmd_sufix = '.conf tests\\12test_1000_500.in '
+
+for i in [4,6,8,9,11,19]:
+    #print (base_cmd+str(i)+base_cmd_sufix)
+    os.system(base_cmd+str(i)+base_cmd_sufix)#"python3 main.py --algorithm all --parameters conf\\param2.conf tests\\01test_20_100.in ")
